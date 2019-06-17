2797c32c15cd7fc175d37ca8ef8b4f4ab1a1f6a0
7h1b0 <7h1b0@users.noreply.github.com>
2019-06-17 22:36:23 +0200

    feat: update last step

--- src/components/steps/Change.tsx
+++ src/components/steps/Change.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Input from 'components/Input';
+import { css } from '@emotion/core';
+
+import { colorSubtitle, sizeText, spaceM } from 'modules/theme';
 
 type Props = {
   euro: number;
@@ -15,25 +18,42 @@ const Change: React.FC<Props> = ({
   currencyLabel,
 }) => (
   <>
-    <Input
-      name="euro"
-      label="Euros"
-      type="number"
-      required
-      value={euro}
-      onChange={onChange}
-      placeholder="Example: 10"
-    />
+    <div
+      css={css`
+        display: flex;
+      `}
+    >
+      <Input
+        name="euro"
+        label="Euros"
+        type="number"
+        required
+        value={euro}
+        onChange={onChange}
+        placeholder="Example: 10"
+      />
 
-    <Input
-      name="currency"
-      label={currencyLabel}
-      type="number"
-      required
-      value={currency}
-      placeholder="Example: 13"
-      onChange={onChange}
-    />
+      <Input
+        name="currency"
+        label={currencyLabel}
+        type="number"
+        required
+        value={currency}
+        placeholder="Example: 13"
+        onChange={onChange}
+      />
+    </div>
+
+    <p
+      css={css`
+        color: ${colorSubtitle};
+        font-size: ${sizeText};
+        text-align: center;
+        margin-top: ${spaceM};
+      `}
+    >
+      {`${euro} euros equals ${currency} ${currencyLabel}`}
+    </p>
   </>
 );
 
