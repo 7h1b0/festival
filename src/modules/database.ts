ea7f32e4664093585543b042aa0ce6d6b373444a
7h1b0 <7h1b0@users.noreply.github.com>
2019-07-10 22:24:33 +0200

    chore: remove console.log

--- src/modules/database.ts
+++ src/modules/database.ts
@@ -4,22 +4,18 @@ const CURRENCIES = 'currencies';
 const PREFERENCES = 'lastUsedCurrency';
 
 export function store(data: Currency[]): void {
-  console.log('store called');
   localStorage.setItem(CURRENCIES, JSON.stringify(data));
 }
 
 export function getAllCurrencies(): Currency[] {
-  console.log('getAllCurrencie called');
   const store = localStorage.getItem(CURRENCIES);
   return (store && JSON.parse(store)) || [];
 }
 
 export function setAsLastUsed(id: number) {
-  console.log('setAsLastUsed called');
   return localStorage.setItem(PREFERENCES, '' + id);
 }
 
 export function getLastUsed() {
-  console.log('getLastUsed called');
-  return parseInt(localStorage.getItem(PREFERENCES) || '', 10);
+  return parseInt(localStorage.getItem(PREFERENCES) || '-1', 10);
 }
