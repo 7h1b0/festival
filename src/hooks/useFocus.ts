f4a11ab6b77f9c26c5a73e3e1373ad51d84daecf
7h1b0 <7h1b0@users.noreply.github.com>
2020-03-21 17:16:41 +0100

    chore(lint): remove eslint-config-preact

--- src/hooks/useFocus.ts
+++ src/hooks/useFocus.ts
@@ -2,9 +2,7 @@ import { useEffect, useRef } from 'preact/hooks';
 
 function useFocus(deps: ReadonlyArray<number>) {
   const inputEl = useRef<HTMLInputElement>(null);
-  useEffect(() => {
-    inputEl.current?.focus();
-  }, deps); // eslint-disable-line
+  useEffect(() => inputEl.current?.focus(), deps); // eslint-disable-line
   return inputEl;
 }
 
