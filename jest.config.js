f71c7367b94a45980e9aea89c5b2e7810ae773c5
7h1b0 <7h1b0@users.noreply.github.com>
2020-04-30 22:53:20 +0200

    test: remove coverage threshold and add test on festival context

--- jest.config.js
+++ jest.config.js
@@ -8,12 +8,4 @@ module.exports = {
   automock: false,
   clearMocks: true,
   errorOnDeprecated: true,
-  coverageThreshold: {
-    global: {
-      branches: 90,
-      functions: 90,
-      lines: 90,
-      statements: 90,
-    },
-  },
 };
