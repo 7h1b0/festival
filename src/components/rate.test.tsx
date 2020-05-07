6c5c5ad60c4a98e6bf19e80d51a150ebe0347ce2
7h1b0 <7h1b0@users.noreply.github.com>
2020-05-07 23:14:33 +0200

    chore: upgrade dependencies

--- src/components/rate.test.tsx
+++ src/components/rate.test.tsx
@@ -6,18 +6,18 @@ import Rate from './rate';
 
 describe('Rate', () => {
   it('should render correctly', () => {
-    const { queryByText } = render(
+    const { getByText } = render(
       <Rate rate={2} source="Jest" target="React" />,
     );
 
-    expect(queryByText('1 Jest = 2 React')).toBeVisible();
+    expect(getByText('1 Jest = 2 React')).toBeVisible();
   });
 
   it('should format nicely', () => {
-    const { queryByText } = render(
+    const { getByText } = render(
       <Rate rate={1 / 3} source="Jest" target="React" />,
     );
 
-    expect(queryByText('1 Jest = 0.33 React')).toBeVisible();
+    expect(getByText('1 Jest = 0.33 React')).toBeVisible();
   });
 });
