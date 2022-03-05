e94327abe8275c5c371bfa97ce69d1b945e68ac4
7h1b0 <7h1b0@users.noreply.github.com>
2022-03-05 15:16:55 +0100

    feat: update design

--- src/lib/Converter.svelte
+++ src/lib/Converter.svelte
@@ -37,7 +37,27 @@
     {value}
     on:input={handleCurrency}
   />
-  <hr class="h-px my-6 border-indigo-600" />
+
+  <div class="flex items-center my-2">
+    <hr class="h-px my-6 border-indigo-600 grow" />
+    <div class="rounded-full bg-indigo-600 text-white p-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-4 w-4"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4"
+        />
+      </svg>
+    </div>
+    <hr class="h-px border-indigo-600 grow" />
+  </div>
 
   <Label
     htmlFor="euros"
