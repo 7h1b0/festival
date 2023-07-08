abd53a9d4a0610032fe7581dab875a416e9ffb55
7h1b0 <7h1b0@users.noreply.github.com>
2023-07-08 16:05:26 +0200

    fix: add condition to rate.js

--- src/rate/rate.js
+++ src/rate/rate.js
@@ -1,6 +1,8 @@
 const searchParams = new URLSearchParams(window.location.search);
 const currency = searchParams.get('currency');
 
-document.getElementById('currency').value = currency;
-document.getElementById('festivalName').value = searchParams.get('name');
-document.getElementById('value').insertAdjacentHTML('afterbegin', currency);
+if (document.getElementById('currency')) {
+  document.getElementById('currency').value = currency;
+  document.getElementById('festivalName').value = searchParams.get('name');
+  document.getElementById('value').insertAdjacentHTML('afterbegin', currency);
+}
